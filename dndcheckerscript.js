5d17272837dba4c7ab8880b759116820caa634f3
moses21 <moses21@student.ubc.ca>
2023-10-05 17:48:09 -0700

    implemented checklist functionality

--- dndcheckerscript.js
+++ dndcheckerscript.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const list = document.querySelector('ul');
+list.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+}, false);
