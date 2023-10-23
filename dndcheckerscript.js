242e2896a94edbe253ced894dabd8d7519ff266a
moses21 <moses21@student.ubc.ca>
2023-10-22 20:32:19 -0700

    finished css of checklist reformat

--- dndcheckerscript.js
+++ dndcheckerscript.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let list = document.querySelector('#checkul');
+let list = document.querySelector('.checkul');
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
