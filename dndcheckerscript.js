80176e2c18bf974f5e7aa2c87e7616c967640128
moses21 <moses21@student.ubc.ca>
2023-10-22 20:39:30 -0700

    fixed javascript for checklist

--- dndcheckerscript.js
+++ dndcheckerscript.js
@@ -1,8 +1,25 @@
 'use strict'
 
-let list = document.querySelector('.checkul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+let list = document.getElementsByClassName('checkul');
+let i;
+for (i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+}
+
+let coll = document.getElementsByClassName('collapsible');
+let j;
+for (j = 0; j < coll.length; j++) {
+    coll[j].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+} 
